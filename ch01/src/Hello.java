023ed884edc20cd53b0edfc52522176c4c7db35d
dbswp <dbswp980427@gmail.com>
2024-07-20 01:39:30 +0900

    자바의 정석 - git과 연동하기

---
+++ ch01/src/Hello.java
@@ -0,0 +1,22 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// ctrl+shift+l 단축키 전체 목록
+		// ctrl+ +,=
+		// ctrl + D 한줄 삭제
+		// ctrl + alt + down 행단위 복사
+		// ctrl + shift + A 멀티컬럼 편집
+		// alt + up,down 행단위 이동
+		// ctrl + i 자동으로 들여쓰기
+		// ctrl + / 주석처리
+		// ctrl + space 자동완성
+		
+		
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+		System.out.println("Hello java");
+	}
+
+}  
